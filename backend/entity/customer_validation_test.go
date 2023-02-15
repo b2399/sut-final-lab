9de5721a91c8a186e8baa217862f1d6a6f8a6b92
unknown <butsarakam2824@gmail.com>
2023-02-15 10:18:17 -0800

    create unit test name not blank - close #3

--- backend/entity/customer_validation_test.go
+++ backend/entity/customer_validation_test.go
@@ -10,7 +10,7 @@ import (
 
 type Customer struct {
 	gorm.Model
-	Name       string // ต้องไม่เป็นค่าว่าง
+	Name       string `valid:"required~name cannot blank"`
 	Email      string
 	CustomerID string // รหัสลูกค้าขึ8นต้นด้วย L หรือ M หรือ H แล้วตามด้วยตัวเลขจํานวน 7 ตัว
 }
@@ -32,4 +32,22 @@ func TestCustomerPass(t *testing.T) {
 		g.Expect(err).To(BeNil())
 
 	})
+
+	t.Run("check name not blank", func(t *testing.T) {
+		c := Customer{
+			Name:       "", //ผิด
+			Email:      "butsarakam@gmail.com",
+			CustomerID: "M1234567",
+		}
+
+		ok, err := govalidator.ValidateStruct(c)
+
+		g.Expect(ok).ToNot(BeTrue())
+
+		g.Expect(err).NotTo(BeNil())
+
+		g.Expect(err.Error()).To(Equal("name cannot blank"))
+
+	})
+
 }
